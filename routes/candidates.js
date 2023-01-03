9cb5d3bcb568fa211d64de991f4a3abff5493205
teyim <teyimasobo@gmail.com>
2023-01-03 22:16:43 +0100

    feat(candidate.js): create delete,getone and update endpoints

--- routes/candidates.js
+++ routes/candidates.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Candidate } from "../models/candidate.js";
 const router = express.Router();
 
+//get all candidates
 router.get("/", async (req, res) => {
   try {
     const candidates = await Candidate.find();
@@ -11,6 +12,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+//add candidate
 router.post("/", async (req, res) => {
   const { firstName, bio, campaign, votes, matricule, age, sex, image } =
     req.body;
@@ -31,4 +33,42 @@ router.post("/", async (req, res) => {
     res.json({ message: error });
   }
 });
+
+//get specific candidate
+router.get("/:candidateId", async (req, res) => {
+  const { candidateId } = req.params;
+  try {
+    const candidate = await Candidate.findById(candidateId);
+    res.json(candidate);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//delete specifc candidate
+router.delete("/:candidateId", async (req, res) => {
+  const { candidateId } = req.params;
+  try {
+    const candidate = await Candidate.remove({ _id: candidateId });
+    res.json(candidate);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//update Candidate Info
+router.patch("/:candidateId", async (req, res) => {
+  const { candidateId } = req.params;
+  const { bio, fullName } = req.body;
+  try {
+    const updatedCandidates = await Candidate.updateOne(
+      { _id: candidateId },
+      { $set: { bio: bio, fullName: fullName } }
+    );
+    res.json(updatedCandidates);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
 export default router;
