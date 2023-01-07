4627b6f4e2272307124e6adedb8e3d8234758aa4
teyim <teyimasobo@gmail.com>
2023-01-07 16:01:36 +0100

    feat: add joi voter registration and login validation

---
+++ helpers/validation.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+
+export const voterRegistrationValidation = (data) => {
+  const schema = Joi.object({
+    fullName: Joi.string().min(6).required(),
+    matricule: Joi.string().min(10).max(10).required(),
+    campaigns: Joi.array().required(),
+    email: Joi.string()
+      .min(6)
+      .required()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } }),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
+
+export const voterLoginValidation = (data) => {
+  const schema = Joi.object({
+    matricule: Joi.string().min(10).max(10).required(),
+    password: Joi.string().min(6).required(),
+  });
+  return schema.validate(data);
+};
