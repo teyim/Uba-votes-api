8ecb1460a11bab4c5f2e93a9a15f95ac3bc3496d
teyim <teyimasobo@gmail.com>
2023-01-11 23:19:45 +0100

    refactor: modify verifyToken

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,14 +1,33 @@
 import jwt from "jsonwebtoken";
 
-export const verifyToken = (req, res, next) => {
+export const verifyUserToken = (req, res, next) => {
   const token = req.header("auth-token");
-  if (!token) return res.status(401).send("Access denied");
+  if (!token)
+    return res
+      .status(401)
+      .send("Access denied. Please login to carryout this operation");
 
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
     next();
   } catch (error) {
-    res.status(400).send("Invalid");
+    res.status(400).send("Authenticate to carry out operation");
+  }
+};
+
+export const verifyAdminToken = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token)
+    return res
+      .status(401)
+      .send("Access denied. Only Admin can carry out this operation");
+
+  try {
+    const verified = jwt.verify(token, process.env.ADMIN_TOKEN_SECRET);
+    req.admin = verified;
+    next();
+  } catch (error) {
+    res.status(400).send("Authenticate to carry out operation");
   }
 };
