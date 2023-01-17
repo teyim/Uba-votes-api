f95c9ae02faaddc3f2908255321287bdd478d9f4
teyim <teyimasobo@gmail.com>
2023-01-17 20:23:12 +0100

    refactor(verifyToken.js):modify file

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -19,15 +19,15 @@ export const verifyUserToken = (req, res, next) => {
 export const verifyAdminToken = (req, res, next) => {
   const token = req.header("auth-token");
   if (!token)
-    return res
-      .status(401)
-      .send("Access denied. Only Admin can carry out this operation");
+    return res.status(401).send("Authenticate to carry out operation");
 
   try {
     const verified = jwt.verify(token, process.env.ADMIN_TOKEN_SECRET);
     req.admin = verified;
     next();
   } catch (error) {
-    res.status(400).send("Authenticate to carry out operation");
+    res
+      .status(400)
+      .send("Access denied. Only Admin can carry out this operation");
   }
 };
