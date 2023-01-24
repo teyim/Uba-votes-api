3481e79784fa3926211f8ec12a567d146b405b46
teyim <teyimasobo@gmail.com>
2023-01-24 17:01:42 +0100

    refactor: modify voters.js routes

--- routes/votes.js
+++ routes/votes.js
@@ -1,19 +1,10 @@
 import express from "express";
-import { addVote, getVotes } from "../controllers/votes.js";
-import {
-  verifyAdminToken,
-  verifyUserToken,
-} from "../middlewares/verifyToken.js";
+import { addVote } from "../controllers/votes.js";
+import { verifyUserToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-//get all votes
-router.get("/", verifyAdminToken, getVotes);
-
 //add vote
 router.post("/", verifyUserToken, addVote);
-
-//get results for specific campiagn..show results only if campaign has ended
-router.get(":/results", () => {});
 
 export default router;
