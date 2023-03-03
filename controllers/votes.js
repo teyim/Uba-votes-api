5b8347de8c45cf1d917b42d678c9bcaa74ec00f5
teyim <teyimasobo@gmail.com>
2023-03-03 15:57:49 +0100

    refactor(votes.js):modify file

--- controllers/votes.js
+++ controllers/votes.js
@@ -4,7 +4,7 @@ import { Voter } from "../models/voter.js";
 const currentDateAndTime = new Date().toISOString();
 
 export const addVote = async (req, res) => {
-  const { voterId, candidates, campaignId } = req.body;
+  const { voterId, candidates, positions, campaignId } = req.body;
 
   try {
     const campaign = await Campaign.findById(campaignId);
@@ -57,36 +57,51 @@ export const addVote = async (req, res) => {
         );
     }
 
-    const hasVoted = voter?.votes?.some(
-      (vote) => vote?.campaignId === campaignId
-    );
+    // const hasVoted = voter?.votes?.some(
+    //   (vote) => vote?.campaignId === campaignId
+    // );
 
-    if (hasVoted) {
-      return res
-        .status(400)
-        .send("Voter has already casted vote for this campaign");
-    }
+    // if (hasVoted) {
+    //   return res
+    //     .status(400)
+    //     .send("Voter has already casted vote for this campaign");
+    // }
 
-    const response = await Campaign.updateMany(
-      { _id: campaignId, "candidates._id": { $in: candidates } },
+    const responseData = await Campaign.updateMany(
+      { _id: campaignId },
       {
         $inc: {
-          "candidates.$[].votes": 1,
+          "votingPositions.$[v].candidates.$[c].votes": 1,
         },
+      },
+      {
+        arrayFilters: [
+          {
+            "v.abbrv": { $in: positions },
+          },
+          {
+            "c._id": { $in: candidates },
+          },
+        ],
       }
     );
 
-    if (!response?.acknowledged) {
-      return res.status(400).send("error update votes");
+    if (responseData?.acknowledged) {
+      candidates?.forEach((candidateId) => {
+        voter?.votes?.push({
+          candidateId: candidateId,
+          campaignId: campaignId,
+        });
+      });
+
+      const updatedVoter = await voter.save();
+
+      return res.send({
+        voter: updatedVoter,
+      });
+    } else {
+      return res.status(400).send("error updating votes");
     }
-
-    candidates?.forEach((candidateId) => {
-      voter?.votes?.push({ candidateId: candidateId, campaignId: campaignId });
-    });
-
-    const updatedVoter = await voter.save();
-
-    res.send(updatedVoter);
   } catch (error) {
     res.status(400).json({ message: error });
   }
